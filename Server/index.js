e05b0d48eae7bdd18ec1ca1df0cac915c243414a
Dronten <drontengaming2004@gmail.com>
2026-09-12 21:04:30 +0530

    user routes add

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,11 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+
+app.use("/api/user", userRoutes);
+
 
 app.get("/", (req, res) => {
     res.send("Death Realm");
