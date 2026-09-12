a133769c2e0294de9cbd014761e40f7eeece8205
Dronten <drontengaming2004@gmail.com>
2026-09-12 20:09:29 +0530

    add cookie and jwt to user controller

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     try {
@@ -14,12 +15,16 @@ export const register = async (req, res) => {
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        await User.create({
+        const newUser = await User.create({
             userName,
             email,
             password: hashedPassword,
             googleId
         });
+
+        const token = await jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+        res.cookie("token", token, { httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: "strict" });
 
         return res.status(201).json({ message: "User created successfully", success: true });
 
@@ -27,7 +32,6 @@ export const register = async (req, res) => {
         return res.status(500).json({ message: error.message, success: false });
     }
 }
-
 
 export const login = async (req, res) => {
     try {
@@ -46,13 +50,16 @@ export const login = async (req, res) => {
             return res.status(400).json({ message: "Incorrect email or password", success: false });
         };
 
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
         user = {
+            _id: user._id,
             userName: user.userName,
             email: user.email,
-            password: user.password,
             googleId: user.googleId
         };
-        return res.status(200).json({ message: "Login successful", success: true, user });
+
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: "strict" }).json({ message: "Login successful", success: true, user });
 
     } catch (error) {
         return res.status(500).json({ message: error.message, success: false });
@@ -61,7 +68,10 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-
+        return res.status(200).cookie("token", '', { maxAge: 0 }).json({
+            message: "Logged out successfully",
+            success: true
+        });
     } catch (error) {
         return res.status(500).json({ message: error.message, success: false });
     }
