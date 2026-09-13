74a90f309478444f0ed5d46e189aaaef904cbbf0
Dronten <drontengaming2004@gmail.com>
2026-09-13 19:57:26 +0530

    Add comments for better readability

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -4,16 +4,19 @@ import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     try {
+        // Extracting user details from the req.body
         const { userName, email, password, googleId } = req.body;
         if (!userName || !email || !password) {
             return res.status(400).json({ message: "Something is missing", success: false });
         }
 
+        // Checking if the user already exists
         const user = await User.findOne({ email });
         if (user) {
             return res.status(400).json({ message: "User already exists", success: false });
         }
 
+        // Hashing the password and creating a new user
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await User.create({
             userName,
@@ -22,11 +25,10 @@ export const register = async (req, res) => {
             googleId
         });
 
-        const token = await jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        // Assigning a JWT token and sending it as cookie in the response
+        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-        res.cookie("token", token, { httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: "strict" });
-
-        return res.status(201).json({ message: "User created successfully", success: true });
+        return res.status(201).cookie("token", token, { httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: "strict" }).json({ message: "User created successfully", success: true });
 
     } catch (error) {
         return res.status(500).json({ message: error.message, success: false });
@@ -40,16 +42,19 @@ export const login = async (req, res) => {
             return res.status(400).json({ message: "Something is missing", success: false });
         };
 
+        // Checking if the user exists
         let user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ message: "Incorrect email or password", success: false });
         };
 
+        // Checking if the password is correct
         const isPasswordMatch = await bcrypt.compare(password, user.password);
         if (!isPasswordMatch) {
             return res.status(400).json({ message: "Incorrect email or password", success: false });
         };
 
+        // Assigning a JWT token and sending it as cookie in the response
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
         user = {
@@ -68,6 +73,7 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
+        // Clearing the token cookie to log the user out
         return res.status(200).cookie("token", '', { maxAge: 0 }).json({
             message: "Logged out successfully",
             success: true
--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -2,14 +2,16 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = async (req, res, next) => {
     try {
+        // Extracting the token from cookies
         const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({ message: "Unauthorized", success: false });
         }
-        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+        //Verifying the token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         req.userId = decoded.userId;
-
         next();
 
     } catch (error) {
