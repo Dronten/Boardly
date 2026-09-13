a7d859b7033a2f1293bd051cf1a6d692487fa103
Dronten <drontengaming2004@gmail.com>
2026-09-13 19:35:20 +0530

    card,board,list controller file made , Auth middleware add

--- Server/index.js
+++ Server/index.js
@@ -2,16 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 
