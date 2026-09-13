53c25f4e497f1c55fa8ae7b5cd2ce2182701d40c
Dronten <drontengaming2004@gmail.com>
2026-09-13 19:39:18 +0530

    typo and status code fix

--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -4,18 +4,15 @@ export const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
         if (!token) {
-            return res.status(401).json({ message: "Unauthorized", sucess: false });
+            return res.status(401).json({ message: "Unauthorized", success: false });
         }
         const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-        if (!decoded) {
-            return res.status(401).json({ message: "Invalid token", success: false });
-        };
 
         req.userId = decoded.userId;
 
         next();
 
     } catch (error) {
-        return res.status(500).json({ message: "Invalid or expired token", success: false });
+        return res.status(401).json({ message: "Invalid or expired token", success: false });
     }
 }
