aca6016054af3d9c05c9385301f12808cb3a94d5
Dronten <drontengaming2004@gmail.com>
2026-09-16 21:47:48 +0530

    List controller start

---
+++ Server/controllers/list.controller.js
@@ -0,0 +1,26 @@
+import { List } from "../models/list.js";
+
+export const createList = async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const updateList = async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const removeList = async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
